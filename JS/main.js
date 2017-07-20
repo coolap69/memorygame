00280c541db9d5222a858fee4a347d80ea83de63
coolap69 <coolap69@aol.com>
2017-07-20 17:15:20 -0400

    update information

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+
+var cardOne = [0];
+cardsInPlay.push('cardOne');
+console.log("User flipped queen");
+
+var cardTwo = [3];
+cardsInPlay.push('cardtwo');
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2); {
+  alert ("you found a match");
+}
+if (cardsInPlay.length == 2){
+  alert ("Sorry try again");
+}
